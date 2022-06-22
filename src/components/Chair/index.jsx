e4df8b2058531894f60e11184f072e426dcf9095
unknown <jamelsala@gmail.com>
2022-06-22 16:30:25 -0300

    ajustes

--- src/components/Chair/index.jsx
+++ src/components/Chair/index.jsx
@@ -1,20 +1,20 @@
+import { isFocusable } from "@testing-library/user-event/dist/utils";
 import React from "react";
 import "./index.css";
-
 
 const Chair = (props) => {
   const id= props.id;
   const [state, setState] = React.useState(props.state);
 
   function handleClick() {
-    if(id == 1) {
+    if(state == 0) {
+      setState(1);
+    } else if(state == 1) {
       setState(0);
     }
-    
-    if(id == 0) {
-      setState(1);
-    }
-   }
+
+  }
+
   return <div className={`${id} chair chair--${state}`} onClick={handleClick}></div>;
 };
 
